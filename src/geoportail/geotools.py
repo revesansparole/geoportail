7053bdc711f0b7a57baa9f419b779a381a503f21
jchopard <jerome.chopard@itk.fr>
2017-07-15 15:43:24 +0200

    added tile resolyution

--- src/geoportail/geotools.py
+++ src/geoportail/geotools.py
@@ -17,3 +17,16 @@ def latlong_to_tile_coords(zoom, latitude, longitude):
     tile_col = int((longitude + 180.0) / 360.0 * n)
     tile_row = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
     return tile_col, tile_row
+
+
+def tile_resolution(zoom, latitude):
+    """Compute resolution for a given position
+
+    Args:
+        zoom (int): zoom level
+        latitude (float): latitude in degrees
+
+    Returns:
+        (float): [m.pix-1] number of meters per pixel in the tile
+    """
+    return 156543.034 * math.cos(latitude) / (2 ** zoom)
